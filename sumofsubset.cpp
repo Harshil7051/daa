b05156789767a5ed35ccc13562a517bae0696647
Harshil <harshilgarg1507@gmail.com>
2024-05-08 20:38:31 +0530

    Update sumofsubset.cpp

--- sumofsubset.cpp
+++ sumofsubset.cpp
@@ -29,16 +29,10 @@ int set[] = { 1, 2, 1 };
 int sum = 3;
 int n = sizeof(set) / sizeof(set[0]);
 vector<int> subset;
-cout << "Output 1:" << endl;
+cout << "Output :" << endl;
 PrintSubsetSum(0, n, set, sum, subset);
 cout << endl;
 flag = 0;
-int set2[] = { 3, 34, 4, 12, 5, 2 };
-int sum2 = 30;
-int n2 = sizeof(set) / sizeof(set[0]);
-vector<int> subset2;
-cout << "Output 2:" << endl;
-PrintSubsetSum(0, n2, set2, sum2, subset2);
 if (!flag) {
 cout << "There is no such subset";
 }
